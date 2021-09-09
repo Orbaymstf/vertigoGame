42398c231e3e9bbf81c719c59d9ce4973ef0f344
Mustafa Orbay <orbaymstf@gmail.com>
2021-09-09 22:42:15 +0300

    + created first lesson scripts

---
+++ Assets/Script/merhaba.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LessonBasıc
+{
+    public class merhaba : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public int sayı2 = 10;
+        [SerializeField] int sayı = 1000;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
